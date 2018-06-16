abf086974d16f627d948d102e8aaffae4b097e8b
brainiac6 <giorgosbrainaic@hotmail.com>
2018-06-16 20:41:48 +0300

    Ilikia kai onoma gia poto

--- src/main.java
+++ src/main.java
@@ -1,2 +1,34 @@
-public class main {
+import java.util.Scanner;
+
+public class main
+{
+    public static String onoma;
+    public static int ilikia;
+    public static Scanner sc;
+    public static void main(String []args)
+    {
+        // Onoma:   Ilikia:  + mporis/ en mporis na peeis poto
+        // Onoma: Sotiris Ilikia: 24 mporis na peeis poto na kamoume to error tis ilikias
+
+        sc = new Scanner(System.in);
+
+        System.out.println("pe m to onoma sou");
+        onoma=sc.next();
+        System.out.println("pe m thn ilikia sou");
+        ilikia=sc.nextInt();
+        System.out.print("Onoma:" + onoma + " "+"Ilikia:" + ilikia + " ");
+        checkare();
+
+    }
+    public static void checkare(){
+        String mporis;
+        if(ilikia>=19)
+        {
+            mporis="mporis";
+        }else{
+            mporis="en mporis";
+        }
+        System.out.println( mporis +" na peeis poto");
+    }
+
 }
